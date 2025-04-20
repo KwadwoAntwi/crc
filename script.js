9dee02a0a0019a3df7868b1abc0d4062b399fcfd
Kwadwo Antwi Appiagyei <mr.k.appiagyei@gmail.com>
2025-04-20 11:50:20 +0000

    Update script.js

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
-
+//order send to mail
   document.getElementById('orderForm').addEventListener('submit', function(e) {
     e.preventDefault();
   
@@ -60,6 +60,30 @@ document.addEventListener("DOMContentLoaded", () => {
     window.location.href = mailtoLink;
   
     closeModal();
+  });
+
+
+
+  //contact us to mail
+  document.getElementById('contactForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+  
+    const name = this.querySelector('input[placeholder="Full Name"]').value;
+    const email = this.querySelector('input[placeholder="Email"]').value;
+    const phone = this.querySelector('input[placeholder="Phone Number"]').value;
+    const topic = this.querySelector('input[placeholder="Subject"]').value;
+    const message = this.querySelector('textarea').value;
+  
+    const subject = encodeURIComponent(topic);
+    const body = encodeURIComponent(
+      `Name: ${name}\nPhone: ${phone}\nEmail: ${email}\nMessage: ${message}`
+    );
+  
+    const mailtoLink = `mailto:40thanniversarycrc@gmail.com?subject=${subject}&body=${body};`
+  
+    window.location.href = mailtoLink;
+  
+    closeModal(); 
   });
 
 
